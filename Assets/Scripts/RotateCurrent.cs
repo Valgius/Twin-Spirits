fc4131656d49767e6ba5b5cb909d202988b31a14
Etheratic <136394417+Etheratic@users.noreply.github.com>
2024-10-10 10:37:52 +1100

    playtesting

--- Assets/Scripts/RotateCurrent.cs
+++ Assets/Scripts/RotateCurrent.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class RotateCurrent : MonoBehaviour
 {
     public GameObject rotatingCurrent;
+    //seperates the two triggers so one will stop the player from backtracking
+    public bool isBackTrackStopper;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            rotatingCurrent.transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.U))
+    //    {
+    //        rotatingCurrent.transform.eulerAngles = new Vector3(0, 0, 0);
+    //    }
+    //}
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        rotatingCurrent.transform.eulerAngles = new Vector3(0, 0, 0);
-        print("current trigger entered");
+        if (other.CompareTag("Player"))
+        {
+            rotatingCurrent.transform.eulerAngles = new Vector3(0, 0, 0);
+            print("current trigger entered");
 
-        if (other.CompareTag("backtrackStopper"))
+        }
+       
+
+        if (isBackTrackStopper == true)
         {
             rotatingCurrent.transform.eulerAngles = new Vector3(0, 0, 90);
         }
