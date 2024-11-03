7a6e51e29adde51ab18df5b188586eae44949528
Coolinglight <91443307+Coolinglight@users.noreply.github.com>
2024-11-04 00:29:36 +1100

    Ben Sunday Week 6
    Reworked fish script to be independent of other enemy scripts.

--- Assets/Scripts/Enemy/FishAttackBox.cs
+++ Assets/Scripts/Enemy/FishAttackBox.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class FishAttackBox : MonoBehaviour
 {
-    public EnemyAttack enemyAttack;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/Scripts/Enemy/NewFish.cs
+++ Assets/Scripts/Enemy/NewFish.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class NewFish : GameBehaviour
 {
     public PatrolType myPatrol;
-    public EnemyAttack enemyAttack;
+    NewFishAttack newFishAttack;
 
     [Header("Patrol Points")]
     public GameObject pointA;
@@ -15,8 +15,6 @@ public class NewFish : GameBehaviour
     private Rigidbody2D rb;
     public Transform currentPoint;
     private Transform playerSea;
-    private Transform playerLeaf;
-    public Transform closestPlayer;
 
     SpriteRenderer spriteRenderer;
 
@@ -25,34 +23,162 @@ public class NewFish : GameBehaviour
     public float movementSpeed;
     public float chaseSpeed;
     public float attackDistance;
-    private float detectCooldown = 5f;
+    private float detectCountdown = 5f;
     public float detectTime = 5f;
     public float detectDistance;
+    public float attackTimer = 0;
+    public bool hasAttacked;
+
+    public GameObject fishAttackBox;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerSea = GameObject.Find("PlayerSea").GetComponent<Transform>();
-        playerLeaf = GameObject.Find("PlayerLeaf").GetComponent<Transform>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         currentPoint = pointB.transform;
-        movementSpeed = startSpeed;
-        detectCooldown = detectTime;
+        detectCountdown = detectTime;
+        newFishAttack = GetComponent<NewFishAttack>();
+        attackTimer = 0;
+        hasAttacked = false;
     }
 
     void Update()
     {
         float disToPlayer = Vector2.Distance(transform.position, playerSea.transform.position);
 
-        if(disToPlayer <= detectDistance && myPatrol != PatrolType.Attack || myPatrol != PatrolType.Chase)
+        if (disToPlayer < detectDistance && myPatrol == PatrolType.Patrol)
         {
             myPatrol = PatrolType.Detect;
         }
+        else if(disToPlayer > detectDistance)
+            transform.right = currentPoint.position - transform.position;
 
-        if(myPatrol != PatrolType.Attack)
+        switch (myPatrol)
         {
-            Vector2.MoveTowards(transform.position, currentPoint.position, movementSpeed * Time.deltaTime);
+            case PatrolType.Patrol:
+                Patrol();
+                break;
+            case PatrolType.Detect:
+                Detect();
+                break;
+            case PatrolType.Chase:
+                Chase();
+                break;
+            case PatrolType.Attack:
+                Attack();
+                break;
         }
+        
+        if (currentPoint.position.x > transform.position.x && myPatrol != PatrolType.Detect)
+        {
+            transform.right = currentPoint.position - transform.position;
+        }
+        else if(currentPoint.position.x < transform.position.x && myPatrol != PatrolType.Detect)
+        {
+            transform.right = transform.position - currentPoint.position;
+            
+        }
+        if(myPatrol != PatrolType.Detect)
+            FlipSprite();
+        else
+            spriteRenderer.flipX = false;
+    }
+
+    public void Patrol()
+    {
+        movementSpeed = startSpeed;
+        detectTime = 2f;
+        Move();
+
+        float disToWaypoint = Vector2.Distance(transform.position, currentPoint.position);
+
+        if (disToWaypoint < 1.5f)
+        {
+            currentPoint = (currentPoint == pointB.transform) ? pointA.transform : pointB.transform;
+        }
+
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, movementSpeed * Time.deltaTime);
+    }
+
+    void Detect()
+    {
+        transform.right = playerSea.position - transform.position;
+
+
+        movementSpeed = 0f;
+        detectTime -= Time.deltaTime;
+        if(detectTime <= 0)
+        {
+            myPatrol = PatrolType.Chase;
+            
+        }
+        if (Vector2.Distance(transform.position, playerSea.transform.position) > detectDistance && detectCountdown > 0)
+        {
+            myPatrol = PatrolType.Patrol;
+
+        }
+        
+    }
+
+    void Chase()
+    {
+        currentPoint = playerSea.transform;
+        Move();
+        movementSpeed = chaseSpeed;
+
+        if(Vector2.Distance(transform.position, playerSea.transform.position) > detectDistance)
+        {
+            currentPoint = pointA.transform;
+            myPatrol = PatrolType.Patrol;
+        }
+
+        if(Vector2.Distance(transform.position, playerSea.position) < attackDistance)
+        {
+            myPatrol = PatrolType.Attack;
+        }
+    }
+
+    void Attack()
+    {
+        if (!hasAttacked && attackTimer <= 0)
+        {
+            attackTimer = 2f;
+
+        }
+
+        if(attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+            fishAttackBox.SetActive(true);
+        }
+        else
+        {
+            fishAttackBox.SetActive(false);
+        }
+        
+        if(Vector2.Distance(transform.position, playerSea.position) > attackDistance && !hasAttacked)
+        {
+            myPatrol = PatrolType.Chase;
+        }
+    }
+
+    void FlipSprite()
+    {
+        if(currentPoint.position.x < transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+
     }
 }
